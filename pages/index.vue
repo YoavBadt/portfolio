d5aa3bd2d7d479129a61db54938ef2e0d8c9283d
Yoav Badt <yoavbadt@gmail.com>
2025-10-27 11:17:29 +0200

    more acurate

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,20 @@
 <template >
    <main class="indexMainBox">
       <!-- <Mygrid /> -->
-      <div class="indexGalleryItem">
-         <NuxtImg class="indexImg" src="barong.jpg" preload />
+      <div class="indexGalleryItem" :style="{gridColumn: '1/span 2',gridRow: '1/span 2',}">
+         <NuxtImg class="indexImg_h" src="barong.jpg" preload />
+      </div>
+      <div class="indexGalleryItem" :style="{gridColumn: '5/span 2',gridRow: '3/span 2',}">
+         <NuxtImg class="indexImg_h" src="blue2.jpg" preload />
+      </div>
+      <div class="indexGalleryItem" :style="{gridColumn: '5/span 2',gridRow: '1/span 2',}">
+         <NuxtImg class="indexImg_h" src="sanhedrin.png" preload />
+      </div>
+      <div class="indexGalleryItem" :style="{gridColumn: '3/span 2',gridRow: '1/span 2',}">
+         <NuxtImg class="indexImg_h" src="fsight1.png" preload />
+      </div>
+        <div class="indexGalleryItem" :style="{gridColumn: '3/span 2',gridRow: '3/span 2',}">
+         <NuxtImg class="indexImg_w" src="radical1.png" preload />
       </div>
 
       
@@ -11,7 +23,7 @@
             <div class="indexMain_wrap">
 
                <h1 class="indexMain_h1">Hello, My name is Yoav Badt.</h1>
-               <h2 class="indexMain_h2">I am a graphic artist & web designer. I code with Javascript and build with <a target="_blank" href="https://vuejs.org">VueJs</a>. I am also a painter & illustrator. I made this Jamstack portfolio website with <a target="_blank" href="https://nuxt.com">Nuxt</a>,  <a target="_blank" href="https://content.nuxt.com">@Nuxt/Content</a>, and deployed it from Github to <a target="_blank" href="https://www.netlify.com/">Netlify</a>.</h2>
+               <h2 class="indexMain_h2">I am a graphic artist & web designer. I code with Javascript and build with <a target="_blank" href="https://vuejs.org">VueJs</a>. I am also a painter & illustrator. I made this Jamstack portfolio website with <a target="_blank" href="https://nuxt.com">Nuxt.js</a>,  <a target="_blank" href="https://content.nuxt.com">@Nuxt/Content</a>, and deployed it from Github to <a target="_blank" href="https://www.netlify.com/">Netlify</a>.</h2>
             </div>
             <!-- <h1 class="indexMain_h1">{{ doc.title}}</h1> -->
             <!-- <h2 class="indexMain_h2">{{ doc.subtitle}}</h2> -->
@@ -36,17 +48,24 @@ export default {
          outline: 1px dotted lightgrey;
          overflow:hidden;
          border-radius:3px;
-         grid-column: 1/span 2;
-         grid-row: 1/span 2;
+         
          display: flex;
          align-items: center;
          justify-content: center;
          /* background:red; */
+         box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
       }
-     .indexImg{
+     .indexImg_h{
+      
         height:100%;
-        
+        /* object-fit : cover; */
      }
+     .indexImg_w{
+     
+      width:100%;
+      /* object-fit : fill; */
+     }
+
      .indexMainBox{
          grid-template-columns: repeat(6,1fr);
          grid-template-rows: repeat(4,1fr);
@@ -56,7 +75,7 @@ export default {
     .indexMain{
       outline: 1px dotted lightgrey;
       border-radius:3px; 
-      /* padding:40px; */
+      padding:40px;
 
       /* font-size:24px;
       line-height:  */
